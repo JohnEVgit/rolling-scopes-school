2c24a7706db3dc7ac33e9a8ca8e33769b66870ec
John <john13ev@gmail.com>
2020-12-07 02:15:56 +0300

    feat: add Summer (set B), Winter cards

--- english-for-kids/assets/js/cards.js
+++ english-for-kids/assets/js/cards.js
@@ -3,7 +3,14 @@ const categoryes = [
         category: 'Summer (set A)',
         image: 'assets/img/beach.svg'
     },
-
+    {
+        category: 'Summer (set B)',
+        image: 'assets/img/sun.svg'
+    },
+    {
+        category: 'Winter',
+        image: 'assets/img/snowman.svg'
+    },
 ];
 const cards = [
     [
@@ -68,7 +75,130 @@ const cards = [
             audioSrc: 'assets/audio/pronunciation_en_lemonade.mp3'
         }
     ],
-
+    [
+        {
+            word: 'picnic',
+            translation: 'пикник',
+            image: 'assets/img/picnic.svg',
+            audioSrc: 'assets/audio/pronunciation_en_picnic.mp3'
+        },
+        {
+            word: 'pool',
+            translation: 'бассейн',
+            image: 'assets/img/pool.svg',
+            audioSrc: 'assets/audio/pronunciation_en_pool.mp3'
+        },
+        {
+            word: 'sailboat',
+            translation: 'парусная лодка',
+            image: 'assets/img/sailboat.svg',
+            audioSrc: 'assets/audio/pronunciation_en_sailboat.mp3'
+        },
+        {
+            word: 'sandcastle',
+            translation: 'замок из песка',
+            image: 'assets/img/sandcastle.svg',
+            audioSrc: 'assets/audio/pronunciation_en_sandcastle.mp3'
+        },
+        {
+            word: 'sun',
+            translation: 'солнце',
+            image: 'assets/img/sun.svg',
+            audioSrc: 'assets/audio/pronunciation_en_sun.mp3'
+        },
+        {
+            word: 'sunglasses',
+            translation: 'солнцезащитные очки',
+            image: 'assets/img/sunglasses.svg',
+            audioSrc: 'assets/audio/pronunciation_en_sunglasses.mp3'
+        },
+        {
+            word: 'surfing',
+            translation: 'серфинг',
+            image: 'assets/img/surfing.svg',
+            audioSrc: 'assets/audio/pronunciation_en_surfing.mp3'
+        },
+        {
+            word: 'swimming',
+            translation: 'плавание',
+            image: 'assets/img/swimming.svg',
+            audioSrc: 'assets/audio/pronunciation_en_swimming.mp3'
+        },
+        {
+            word: 'trunks',
+            translation: 'плавки',
+            image: 'assets/img/trunks.svg',
+            audioSrc: 'assets/audio/pronunciation_en_trunks.mp3'
+        },
+        {
+            word: 'watermelon',
+            translation: 'арбуз',
+            image: 'assets/img/watermelon.svg',
+            audioSrc: 'assets/audio/pronunciation_en_watermelon.mp3'
+        }
+    ],
+    [
+        {
+            word: 'boots',
+            translation: 'сапоги',
+            image: 'assets/img/boots.svg',
+            audioSrc: 'assets/audio/pronunciation_en_boots.mp3'
+        },
+        {
+            word: 'coat',
+            translation: 'пальто',
+            image: 'assets/img/coat.svg',
+            audioSrc: 'assets/audio/pronunciation_en_coat.mp3'
+        },
+        {
+            word: 'mittens',
+            translation: 'рукавицы, варежки',
+            image: 'assets/img/mittens.svg',
+            audioSrc: 'assets/audio/pronunciation_en_mittens.mp3'
+        },
+        {
+            word: 'ice',
+            translation: 'лёд',
+            image: 'assets/img/ice.svg',
+            audioSrc: 'assets/audio/pronunciation_en_ice.mp3'
+        },
+        {
+            word: 'scarf',
+            translation: 'шарф',
+            image: 'assets/img/scarf.svg',
+            audioSrc: 'assets/audio/pronunciation_en_scarf.mp3'
+        },
+        {
+            word: 'icicle',
+            translation: 'сосулька',
+            image: 'assets/img/icicle.svg',
+            audioSrc: 'assets/audio/pronunciation_en_icicle.mp3'
+        },
+        {
+            word: 'sled',
+            translation: 'санки',
+            image: 'assets/img/sled.svg',
+            audioSrc: 'assets/audio/pronunciation_en_sled.mp3'
+        },
+        {
+            word: 'snowflake',
+            translation: 'снежинка',
+            image: 'assets/img/snowflake.svg',
+            audioSrc: 'assets/audio/pronunciation_en_snowflake.mp3'
+        },
+        {
+            word: 'snowman',
+            translation: 'снеговик',
+            image: 'assets/img/snowman.svg',
+            audioSrc: 'assets/audio/pronunciation_en_snowman.mp3'
+        },
+        {
+            word: 'sweater',
+            translation: 'свитер',
+            image: 'assets/img/sweater.svg',
+            audioSrc: 'assets/audio/pronunciation_en_sweater.mp3'
+        }
+    ],
 ];
 
 
