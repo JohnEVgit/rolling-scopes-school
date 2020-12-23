f8af76041fb438d06788234245af54fa802c9435
John <john13ev@gmail.com>
2020-12-23 03:58:22 +0300

    feat: info table

--- covid-dashboard/js/main.js
+++ covid-dashboard/js/main.js
@@ -1,5 +1,5 @@
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 3);
+let mymap = L.map('mapid').setView([51.505, -0.09], 3);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
@@ -10,26 +10,19 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1
 }).addTo(mymap);
 
-
 const getCountriesList = () => {
     fetch('https://disease.sh/v3/covid-19/countries')
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-
             renderCountriesList(data);
-
         });
 };
 
 getCountriesList();
 
-const renderCountriesList = (data) => {
-    console.log(data);
-    const countriesListCont = document.querySelector('.countries-list');
-    const countriesListFragment = document.createDocumentFragment();
-
+const sortCasesByCountry = (data) => {
     let totalCasesByCountry = [];
     data.forEach((item) => {
         totalCasesByCountry.push( [item.country, item.cases] );
@@ -37,6 +30,14 @@ const renderCountriesList = (data) => {
     totalCasesByCountry.sort(function(a, b) {
         return b[1] - a[1] ;
     });
+    return totalCasesByCountry;
+};
+
+const renderCountriesList = (data) => {
+    const countriesListCont = document.querySelector('.countries-list');
+    const countriesListFragment = document.createDocumentFragment();
+
+    const totalCasesByCountry = sortCasesByCountry(data);
 
     totalCasesByCountry.forEach((item) => {
 
@@ -56,7 +57,84 @@ const renderCountriesList = (data) => {
         countriesListFragment.appendChild(countriesListElem);
 
     });
-debugger
+
     countriesListCont.appendChild(countriesListFragment);
 
 };
+
+
+const getWorldList = (info) => {
+    fetch('https://disease.sh/v3/covid-19/all')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            renderWorldList(data,info);
+        });
+};
+
+const renderWorldListHead = () => {
+    const worldListTable = document.querySelector('.info-table');
+
+    worldListTable.innerHTML = `<tr>
+                    <th>Cases</th>
+                    <th>Deaths</th>
+                    <th>Recovered</th>
+                </tr>`
+};
+
+const renderWorldList = (data,info) => {
+
+    let textContentMass;
+    switch (info) {
+        case 'total':
+            textContentMass = [data.cases, data.deaths, data.recovered];
+            break;
+        case 'totalLastDay':
+            textContentMass = [data.todayCases, data.todayDeaths, data.todayRecovered];
+            break;
+        case 'totalOneThousand':
+            textContentMass = [data.casesPerOneMillion / 1000, data.deathsPerOneMillion / 1000, data.recoveredPerOneMillion / 1000, ];
+            break;
+        case 'totalOneThousandLastDay':
+            textContentMass = [data.todayCases/(data.population / 1000), data.todayDeaths/(data.population / 1000), data.todayRecovered/(data.population / 1000), ];
+            break;
+    }
+
+    if (textContentMass) {
+        textContentMass = textContentMass.map(function(val) {
+            return (Math.round(val * 100000)) / 100000;
+        });
+    }
+
+    const worldListTable = document.querySelector('.info-table');
+    const worldListElem = document.createElement('tr');
+
+    renderWorldListHead();
+
+    const casesVal = document.createElement('td');
+    casesVal.textContent = textContentMass ? textContentMass[0] : data.cases;
+
+    const deathsVal = document.createElement('td');
+    deathsVal.textContent = textContentMass ? textContentMass[1] : data.deaths;
+
+    const recoveredVal = document.createElement('td');
+    recoveredVal.textContent = textContentMass ? textContentMass[2] : data.recovered;
+
+    worldListElem.appendChild(casesVal);
+    worldListElem.appendChild(deathsVal);
+    worldListElem.appendChild(recoveredVal);
+    worldListTable.appendChild(worldListElem);
+
+};
+
+getWorldList();
+
+const infoTableBtns = document.querySelectorAll('.info-table-btn');
+infoTableBtns.forEach(function (button) {
+
+    button.addEventListener("click", function () {
+        getWorldList(this.dataset.info);
+    });
+
+});
